d9640f97d47213456d26614e3cb22f0fea744b1e
trashyfish <ayodelebabajidechris123@gmail.com>
2024-01-30 03:59:49 +0100

    fourth change orderSchema

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -5,6 +5,7 @@ const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   lastName: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true, unique: true },
   photo: { type: String, required: true },
 });
 

ab411b636f15d3b32528f1def1aaa58e1bc52966
trashyfish <ayodelebabajidechris123@gmail.com>
2024-02-11 17:07:43 +0100

    utils actions update

--- lib/utils.ts
+++ lib/utils.ts
@@ -60,7 +60,7 @@ export const formatPrice = (price: string) => {
   const amount = parseFloat(price);
   const formattedPrice = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'NGN',
   }).format(amount);
 
   return formattedPrice;

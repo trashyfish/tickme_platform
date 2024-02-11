a093acceb638ea1f2d9cf551223375ce9672f196
trashyfish <ayodelebabajidechris123@gmail.com>
2024-02-11 06:01:53 +0100

    implement stripe6

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 import { WebhookEvent } from '@clerk/nextjs/server';
-import { createUser, updateUser, deleteUser } from '@/lib/actions/user.actions';
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions';
 import { clerkClient } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
@@ -77,34 +77,30 @@ export async function POST(req: Request) {
         },
       });
     }
-
     return NextResponse.json({ message: 'OK', user: newUser });
   }
 
   if (eventType === 'user.updated') {
-    const { id, username, first_name, last_name, image_url } = evt.data;
+    const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      username: username!,
       firstName: first_name,
       lastName: last_name,
+      username: username!,
       photo: image_url,
     };
 
-    const newUpdateUser = await updateUser(id, user);
+    const updatedUser = await updateUser(id, user);
 
-    return NextResponse.json({ message: 'OK', user: newUpdateUser });
+    return NextResponse.json({ message: 'OK', user: updatedUser });
   }
 
   if (eventType === 'user.deleted') {
     const { id } = evt.data;
 
-    const newDeleteUser = await deleteUser(id!);
+    const deletedUser = await deleteUser(id!);
 
-    return NextResponse.json({
-      message: 'User is deleted',
-      user: newDeleteUser,
-    });
+    return NextResponse.json({ message: 'OK', user: deletedUser });
   }
 
   return new Response('', { status: 200 });
